5d329caf2894901c40c316c8ed7891f178f31a36
hoggar bismark <hoggarbkonline@gmail.com>
2023-06-21 05:23:25 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Makes alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char le;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (le = 'a'; le <= 'z'; le++)
+	{
+	_putchar(le);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
