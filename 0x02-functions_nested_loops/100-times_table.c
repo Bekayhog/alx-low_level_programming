57de524ee83294e8b3e689ea4310f787da7ed695
hoggar bismark <hoggarbkonline@gmail.com>
2023-06-21 17:52:29 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the n times table
+ *
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
+ */
+
+void print_times_table(int n)
+{
+	int a, b, op;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				op = a * b;
+				if (op <= 99)
+				_putchar(' ');
+				if (op <= 9)
+				_putchar(' ');
+				if (op >= 100)
+				{
+					_putchar((op / 100) + '0');
+					_putchar((op / 10) % 10 + '0');
+				}
+				else if (op <= 99 && op <= 10)
+				{
+					_putchar((op / 100) + '0');
+				}
+				_putchar((op % 10) + '0');
+				}	
+				_putchar('\n');
+				}
+		}
+}
